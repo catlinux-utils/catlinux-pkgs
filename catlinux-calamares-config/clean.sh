86f61b46cce3be7492674e6571080abb43d914df
none <none>
2024-06-09 00:06:17 +0200

    update

---
+++ catlinux-calamares-config/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -fr catlinux-calamares-config  *.pkg.tar.zst  pkg  src
---
+++ catlinux-configs-plasma/clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -fr catlinux-configs-plasma pkg src catlinux-configs-plasma-2-5-any.pkg.tar.zst
