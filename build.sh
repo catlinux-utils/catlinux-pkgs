c630516af1e1d152d1ca8a40f10750da142591b0
ThePolishCat <damianadam000@gmail.com>
2025-04-28 17:30:19 +0200

    add new pkgbuilds, update mirrorlist, fixes,etc

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 main=$(pwd)
 if [ -z "$1" ]; then
-    echo "Please enter the directory"
+    echo "Please enter the directories"
     exit 1
 fi
 
-if [ ! -d "$main/$1" ]; then
-    echo "Directory $1 does not exist"
-    exit 1
-fi
+for dir in $*; do
+    if [ ! -d "$main/$dir" ]; then
+        echo "Directory $dir does not exist"
+        exit 1
+    fi
 
-cd "$main/$1"
-echo "Building $1"
-GPGKEY="E40E7EDDF0FF9143" PACKAGER="ThePolishCat <damianadam000@gmail.com>" makepkg --clean --sign 
-cd "$main"
-
+    cd "$main/$dir"
+    echo "Building $dir"
+    GPGKEY="E40E7EDDF0FF9143" PACKAGER="ThePolishCat <damianadam000@gmail.com>" makepkg --clean --sign --syncdeps
+    cd "$main"
+done
